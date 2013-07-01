7f80393b1a92f3f728d849cf9aebad83fdb729b8
andjc <lang.support@gmail.com>
2013-07-01 11:11:27 +1000

    Merged shan branch bank into master branch.

---
+++ greasemonkey/mymr.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name                jquery.mymr
+// @namespace	        http://www.openroad.net.au/tools/jquery.mymr/
+// @description	        Provide linebreaking for Myanmar script languages
+// @include		*
+// ==/UserScript==
+
