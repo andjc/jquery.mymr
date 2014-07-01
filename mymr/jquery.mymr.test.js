ac08411ce81698926a5b12d929db26cf18b9f361
GreenLikeOrange <beginofalove@hotmail.com>
2014-07-02 04:56:32 +0630

    Add fixed
    * Extend ZWSP length ဿ၌-၏ for "my"
    * Add /([က-အ])\u200B([က-အ])/g for "my"
    * Reduce Process time without access DOM:"body" again and again

--- mymr/jquery.mymr.test.js
+++ mymr/jquery.mymr.test.js
@@ -32,7 +32,7 @@ if (!x) {
 
 
 /*
- * mymrLineBrk(mymrLang)
+ * mymrLineBrk(mymrLang, content)
  * jQuery function to insert ZWSP at syllable boundaries to allow line breaking for Myanmar script languages.
  * Currently supports: Burmese, Mon, S'gaw Karen
  *
@@ -65,112 +65,105 @@ if (!x) {
  */
 
 
-function mymrLineBrk(docLang) {
-       if(docLang != null) {
+function mymrLineBrk(docLang, content) {
+
     switch (docLang) {
-      case "my":
-      case "rki":
-      case "tvn":
-      case "int":
-      // Unicode 5.1 to Unicode 5.2
-      jQuery("body").html(jQuery("body").html().replace(/(\u103A)(\u1037)/g, '$2$1'));
-      // Burmese break points - insert ZWSP before consonants and independant vowels
-      // ([က-အဣ-ဧဩဪ])
-      jQuery("body").html(jQuery("body").html().replace(/([\u1000-\u1021\u1023-\u1027\u1029\u102A])/g, '|$1'));
-      //Suppress unwanted breakpoints
-      jQuery("body").html(jQuery("body").html().replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f]|>|\u201C|\u2018|\-|\(|\[|{|[\u2012-\u2014]|\u1039)|([\u1000-\u1021])/g, '$1$2'));
-      jQuery("body").html(jQuery("body").html().replace(/|(\u1004\u103A\u1039)/g, '$1'));
-      jQuery("body").html(jQuery("body").html().replace(/|([က-အ]\u103A)/g, '$1'));
-      jQuery("body").html(jQuery("body").html().replace(/(\s|\n)|([က-အဣ-ဧဩဪ])/g, '$1$2'));
-      break;
-     
-      case "ksw":
-      // Unicode 5.1 to Unicode 5.2
-      // jQuery("body").html(jQuery("body").html().replace(/(\u103A)(\u1037)/g, '$2$1'));
-      // S'gaw Karen break points
-      jQuery("body").html(jQuery("body").html().replace(/([\u1000-\u1006\u100a\u1010-\u1012\u1014-\u1016\u1018-\u101f\u1021\u1027\u1061])/g, '|$1'));
-      //Suppress unwanted breakpoints
-      jQuery("body").html(jQuery("body").html().replace(/(^|[\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f]|\||>|[\u0021-\u0023\u0025-\u002A\u002C-\u002F\u003A\u003B\u003F\u0040\u005B-\u005D\u005F\u007B\u007D\u00A1\u00A7\u00AB\u00B6\u00B7\u00BB\u00BF\u104A-\u104F\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E]||)\|/g, '$1'));
-      break;
-     
-      case "pwo":
-      break;
-     
-      case "kjp":
-      break;
-     
-      case "blk":
-      break;
-     
-      case "mnw":
-      // Unicode 5.1 to Unicode 5.2
-      jQuery("body").html(jQuery("body").html().replace(/(\u103A)(\u1037)/g, '$2$1'));
-      // Mon break points
-      jQuery("body").html(jQuery("body").html().replace(/([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u1023\u1025\u1028-\u102A\u105A-\u105D])/g, '|$1'));
-      //Suppress unwanted breakpoints
-      jQuery("body").html(jQuery("body").html().replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f]|>|\u201C|\u2018|\-|\(|\[|{|[\u2012-\u2014]|\u1039)|([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u105A-\u105D])/g, '$1$2'));
-      jQuery("body").html(jQuery("body").html().replace(/|([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u105A-\u105D]\u103A)/g, '$1'));
-      jQuery("body").html(jQuery("body").html().replace(/(\s|\n)|([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u1023\u1025\u1028-\u102A\u105A-\u105D])/g, '$1$2'));
-      break;
-     
-      case "kyu":
-      break;
-     
-      case "csh":
-      break;
-     
-      case "shn":
-      // Unicode 5.1 to Unicode 5.2
-      // jQuery("body").html(jQuery("body").html().replace(/(\u103A)(\u1037)/g, '$2$1'));
-      // Shan break points
-      jQuery("body").html(jQuery("body").html().replace(/([\u1004\u1010\u1011\u1015\u1019-\u101E\u1022\u1075-\u1081\u109E\u109F])/g, '|$1'));
-      //Suppress unwanted breakpoints
-      jQuery("body").html(jQuery("body").html().replace(/|([\u1004\u1010\u1011\u1015\u1019-\u101C\u101E\u1022\u1075-\u1079\u107B-\u1081\u109E\u109F]\u103A)/g, '$1'));
-      jQuery("body").html(jQuery("body").html().replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f>\u201C\u2018\-\(\[{\u2012-\u2014])|([\u1004\u1010\u1011\u1015\u1019-\u101E\u1022\u1075-\u1081\u109E\u109F])/g, '$1$2'));
-      break;
-     
-      case "kht":
-      // Unicode 5.1 to Unicode 5.2
-      // jQuery("body").html(jQuery("body").html().replace(/(\u103A)(\u1037)/g, '$2$1'));
-      // Khmati Shan break points
-      jQuery("body").html(jQuery("body").html().replace(/([\u1000\u1002\u1004\u1010\u1011\u1015\u1019-\u101D\u1022\u1075\u1078\u1079\u107B\u107C\u107F\u1080\uAA60-\uAA6F\uAA71-\uAA76])/g, '|jQuery1'));
-      //Suppress unwanted breakpoints
-      jQuery("body").html(jQuery("body").html().replace(/|([\u1000\u1004\u1010\u1015\u1019\u101D\uAA65\uAA6B]\u103A)/g, '$1'));
-      jQuery("body").html(jQuery("body").html().replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f>\u201C\u2018\-\(\[{\u2012-\u2014])|([\u1000\u1002\u1004\u1010\u1011\u1015\u1019-\u101D\u1022\u1075\u1078\u1079\u107B\u107C\u107F\u1080\uAA60-\uAA6F\uAA71-\uAA76])/g, '$1$2'));
-      break;
-     
-      case "aio":
-      case "phk":
-      break;
-     
-      case "tle":
-      break;
-     
-      case "pll":
-      break;
-     
-      case "pce":
-      break;
-     
-      case "rbb":
-      break;
-     
-      default:
-      jQuery("body").html(jQuery("body").html().replace(/(\u103A)(\u1037)/g, '$2$1'));
-      break;
-  }
-  } else {
-    jQuery('*:lang(my)').html().replace(/(\u103A)(\u1037)/g, '$2$1');
-    jQuery('*:lang(my)').html().replace(/([\u1000-\u1021\u1023-\u1027\u1029\u102A])/g, '|$1');
-    jQuery('*:lang(my)').html().replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f]|>|\u201C|\u2018|\-|\(|\[|{|[\u2012-\u2014]|\u1039)|([\u1000-\u1021])/g, 'jQuery1jQuery2');
-    jQuery('*:lang(my)').html().replace(/|(\u1004\u103A\u1039)/g, '$1');
-    jQuery('*:lang(my)').html().replace(/|([က-အ]\u103A)/g, '$1');
-    jQuery('*:lang(my)').html().replace(/(\s|\n)|([က-အဣ-ဧဩဪ])/g, '$1$2');
-  }
+        case "my":
+        case "rki":
+        case "tvn":
+        case "int":
+            // Unicode 5.1 to Unicode 5.2
+            content = content.replace(/(\u103A)(\u1037)/g, '$2$1');
+            // Burmese break points - insert ZWSP before consonants and independant vowels
+            // ([က-အဣ-ဧဩဪဿ၌-၏])
+            content = content.replace(/([က-အဣ-ဧဩဪဿ၌-၏])/g, '\u200B$1');
+            //Suppress unwanted breakpoints
+            content = content.replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f]|>|\u201C|\u2018|\-|\(|\[|{|[\u2012-\u2014]|\u1039)\u200B([\u1000-\u1021])/g, '$1$2');
+            content = content.replace(/\u200B(\u1004\u103A\u1039)/g, '$1');
+            content = content.replace(/\u200B([က-အ]\u103A)/g, '$1');
+            content = content.replace(/(\s|\n)\u200B([က-အဣ-ဧဩဪဿ၌-၏])/g, '$1$2');
+            content = content.replace(/([က-အ])\u200B([က-အ])/g, "$1$2");
+            break;
+ 
+        case "ksw":
+            // Unicode 5.1 to Unicode 5.2
+            // content = content.replace(/(\u103A)(\u1037)/g, '$2$1');
+            // S'gaw Karen break points
+            content = content.replace(/([\u1000-\u1006\u100a\u1010-\u1012\u1014-\u1016\u1018-\u101f\u1021\u1027\u1061])/g, '\u200B$1');
+            //Suppress unwanted breakpoints
+            content = content.replace(/(^|[\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f]|\||>|[\u0021-\u0023\u0025-\u002A\u002C-\u002F\u003A\u003B\u003F\u0040\u005B-\u005D\u005F\u007B\u007D\u00A1\u00A7\u00AB\u00B6\u00B7\u00BB\u00BF\u104A-\u104F\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E]|\u200B)\|/g, '$1');
+            break;
+ 
+        case "pwo":
+            break;
+ 
+        case "kjp":
+            break;
+ 
+        case "blk":
+            break;
+ 
+        case "mnw":
+            // Unicode 5.1 to Unicode 5.2
+            content = content.replace(/(\u103A)(\u1037)/g, '$2$1');
+            // Mon break points
+            content = content.replace(/([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u1023\u1025\u1028-\u102A\u105A-\u105D])/g, '\u200B$1');
+            //Suppress unwanted breakpoints
+            content = content.replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f]|>|\u201C|\u2018|\-|\(|\[|{|[\u2012-\u2014]|\u1039)\u200B([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u105A-\u105D])/g, '$1$2');
+            content = content.replace(/\u200B([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u105A-\u105D]\u103A)/g, '$1');
+            content = content.replace(/(\s|\n)\u200B([\u1000-\u1003\u1005-\u1007\u1009-\u1021\u1023\u1025\u1028-\u102A\u105A-\u105D])/g, '$1$2');
+            break;
+ 
+        case "kyu":
+            break;
+ 
+        case "csh":
+            break;
+ 
+        case "shn":
+            // Unicode 5.1 to Unicode 5.2
+            // content = content.replace(/(\u103A)(\u1037)/g, '$2$1');
+            // Shan break points
+            content = content.replace(/([\u1004\u1010\u1011\u1015\u1019-\u101E\u1022\u1075-\u1081\u109E\u109F])/g, '\u200B$1');
+            //Suppress unwanted breakpoints
+            content = content.replace(/\u200B([\u1004\u1010\u1011\u1015\u1019-\u101C\u101E\u1022\u1075-\u1079\u107B-\u1081\u109E\u109F]\u103A)/g, '$1');
+            content = content.replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f>\u201C\u2018\-\(\[{\u2012-\u2014])\u200B([\u1004\u1010\u1011\u1015\u1019-\u101E\u1022\u1075-\u1081\u109E\u109F])/g, '$1$2');
+            break;
+ 
+        case "kht":
+            // Unicode 5.1 to Unicode 5.2
+            // content = content.replace(/(\u103A)(\u1037)/g, '$2$1');
+            // Khmati Shan break points
+            content = content.replace(/([\u1000\u1002\u1004\u1010\u1011\u1015\u1019-\u101D\u1022\u1075\u1078\u1079\u107B\u107C\u107F\u1080\uAA60-\uAA6F\uAA71-\uAA76])/g, '\u200BjQuery1');
+            //Suppress unwanted breakpoints
+            content = content.replace(/\u200B([\u1000\u1004\u1010\u1015\u1019\u101D\uAA65\uAA6B]\u103A)/g, '$1');
+            content = content.replace(/([\u0009-\u000d\u0020\u00a0\u2000-\u200a\u2028\u2029\u202f>\u201C\u2018\-\(\[{\u2012-\u2014])\u200B([\u1000\u1002\u1004\u1010\u1011\u1015\u1019-\u101D\u1022\u1075\u1078\u1079\u107B\u107C\u107F\u1080\uAA60-\uAA6F\uAA71-\uAA76])/g, '$1$2');
+            break;
+ 
+        case "aio":
+        case "phk":
+            break;
+ 
+        case "tle":
+            break;
+ 
+        case "pll":
+            break;
+ 
+        case "pce":
+            break;
+ 
+        case "rbb":
+            break;
+ 
+        default:
+            content = content.replace(/(\u103A)(\u1037)/g, '$2$1');
+            break;
+    }
+
+    return content;
 
 }
-
-
 
 /*
  * Execute whe document is ready
@@ -178,10 +171,21 @@ function mymrLineBrk(docLang) {
  *
  */
 
-jQuery(document).ready(function () {
-  alert("HELP");
+jQuery(function () {
+
   if (mymrLBFlag) {
-    mymrLineBrk(mymrLang);
-    }
-});   
     
+    if(mymrLang != null){
+        jQuery("body").html( function(i, oldhtml ) {
+            return mymrLineBrk( mymrLang, oldhtml );
+        });
+    }
+    else{
+        jQuery('*:lang(my)').html( function(i, oldhtml) {
+            return mymrLineBrk( "my", oldhtml );
+        });
+    }
+
+  }
+
+});
